30ffc078d8803947ab5d4ddc814b99084af9f517
tretyigo <igor.tret@gmail.com>
2020-08-17 17:07:34 +0300

    Second commit

---
+++ Platformer/Assets/UserPrefab/Player.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    float speed;
+    bool right;
+    Rigidbody2D RB;
+    Vector3 direction;
+    float horizontal;
+
+
+    private void Move()
+    {
+        direction = Vector3.right * horizontal;
+        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+    }
+
+
+    private void Start()
+    {
+        speed = 3;
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        if (Input.GetButton("Horizontal"))
+        {
+            Move();
+        }
+    }
+}
